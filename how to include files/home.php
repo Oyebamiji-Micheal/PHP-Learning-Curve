879d849699d38f4e2e4de940bb06ec865870f8a4
Oyebamiji Micheal Tomiwa <oyebamijimicheal10@gmail.com>
2023-02-21 22:06:49 +0100

    Create home.php

---
+++ how to include files/home.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset='utf-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <title>Home Page</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+    </head>
+    <body>
+        <?php include './utilities/nav.php'?>
+        <h1>Home</h1>
+        <p>Welcome to the home page!</p>
+    </body>
+</html>
