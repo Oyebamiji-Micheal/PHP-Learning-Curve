916d382ae9d9cb39cbfe0f1c9d4d9affc57a9ea1
Oyebamiji Micheal Tomiwa <oyebamijimicheal10@gmail.com>
2023-02-21 22:13:30 +0100

    Create contact.php

---
+++ how to include files/contact.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset='utf-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <title>Contact Page</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+    </head>
+    <body>
+        <?php include './utilities/nav.php'?>
+        <h1>Contact</h1>
+        <p>Welcome to the contact page!</p>
+    </body>
+</html>
