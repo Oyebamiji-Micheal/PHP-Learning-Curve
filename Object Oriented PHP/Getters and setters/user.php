5c34a18fed974d4c4b0435eda8354958f6106239
Oyebamiji Micheal Tomiwa <oyebamijimicheal10@gmail.com>
2023-02-23 21:17:27 +0100

    Create user.php

---
+++ Object Oriented PHP/Getters and setters/user.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+class User
+{
+    private string $name;
+    private int $age;
+
+    public function __construct(string $name, int $age)
+    {
+        $this->name = $name;
+        $this->age = $age;
+    }
+
+    public function __get($property)
+    {
+        if (property_exists($this, $property)) {
+            return $this->$property;
+        }
+    }
+
+    public function __set($property, $value)
+    {
+        if (property_exists($this, $property)) {
+            $this->$property = $value;
+        }
+    }
+}
